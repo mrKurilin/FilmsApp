d5f35f1c0701737c5054c7bf2750c629a60c0e08
MrKurilin <kurilin-anton@mail.ru>
2023-03-21 12:11:42 +0300

    after review

--- app/src/main/java/com/mrkurilin/filmsapp/data/EmailValidation.kt
+++ app/src/main/java/com/mrkurilin/filmsapp/data/EmailValidation.kt
@@ -4,17 +4,17 @@ import java.util.regex.Pattern
 
 class EmailValidation {
 
-    private val pattern: Pattern = Pattern.compile(
-    ("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-    "\\@" +
-    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-    "(" +
-    "\\." +
-    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-    ")+")
-    )
-
     fun isInvalidEmail(email: CharSequence): Boolean {
+        val pattern: Pattern = Pattern.compile(
+            ("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+")
+        )
+
         return !(email.isNotEmpty() && pattern.matcher(email).matches())
     }
 }
