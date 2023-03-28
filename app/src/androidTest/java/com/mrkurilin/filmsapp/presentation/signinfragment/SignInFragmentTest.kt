dab9de694e06692f20c2aeb54450a00f20a311b0
MrKurilin <kurilin-anton@mail.ru>
2023-03-28 10:28:59 +0300

    Cleanup

--- app/src/androidTest/java/com/mrkurilin/filmsapp/presentation/signinfragment/SignInFragmentTest.kt
+++ app/src/androidTest/java/com/mrkurilin/filmsapp/presentation/signinfragment/SignInFragmentTest.kt
@@ -1,13 +1,16 @@
 package com.mrkurilin.filmsapp.presentation.signinfragment
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.mrkurilin.filmsapp.R
@@ -21,27 +24,34 @@ import org.junit.runner.RunWith
 class SignInFragmentTest {
 
     private val validEmail = "Test@test.test"
-    private val validPassword = "Qq12345678"
     private val invalidEmail = "invalidEmail"
+    private val validPassword = "Qq12345678"
     private val invalidPassword = "123"
 
     private lateinit var signInButton: ViewInteraction
     private lateinit var emailEditText: ViewInteraction
     private lateinit var passwordEditText: ViewInteraction
+    private lateinit var signUpTextView: ViewInteraction
+
+    private lateinit var scenario: FragmentScenario<SignInFragment>
 
     @Before
     fun setUp() {
-        launchFragmentInContainer(themeResId = R.style.Theme_FilmsApp) {
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_FilmsApp) {
             SignInFragment()
         }
         signInButton = onView(withId(R.id.sign_in_button))
         emailEditText = onView(withId(R.id.email_edit_text))
         passwordEditText = onView(withId(R.id.password_edit_text))
+        signUpTextView = onView(withId(R.id.sign_up_text_view))
     }
 
     @Test
     fun pressSignInWithEmptyFields() {
         signInButton.perform(click())
+
+        checkSignUpGroupIsDisplayed()
+
         emailEditText.check(matches(hasErrorText(emptyFieldError)))
         passwordEditText.check(matches(hasErrorText(emptyFieldError)))
     }
@@ -49,14 +59,22 @@ class SignInFragmentTest {
     @Test
     fun pressSignInWithEmptyPassword() {
         emailEditText.perform(typeText(validEmail))
+
         signInButton.perform(click())
+
+        checkSignUpGroupIsDisplayed()
+
         passwordEditText.check(matches(hasErrorText(emptyFieldError)))
     }
 
     @Test
     fun pressSignInWithEmptyEmail() {
         passwordEditText.perform(typeText(validPassword))
+
         signInButton.perform(click())
+
+        checkSignUpGroupIsDisplayed()
+
         emailEditText.check(matches(hasErrorText(emptyFieldError)))
     }
 
@@ -64,7 +82,11 @@ class SignInFragmentTest {
     fun pressSignInWithValidEmailAndInvalidPassword() {
         passwordEditText.perform(typeText(invalidPassword))
         emailEditText.perform(typeText(validEmail))
+
         signInButton.perform(click())
+
+        checkSignUpGroupIsDisplayed()
+
         passwordEditText.check(matches(hasErrorText(invalidPasswordError)))
     }
 
@@ -72,8 +94,31 @@ class SignInFragmentTest {
     fun pressSignInWithInvalidEmailAndValidPassword() {
         emailEditText.perform(typeText(invalidEmail))
         passwordEditText.perform(typeText(validPassword))
+
         signInButton.perform(click())
+
+        checkSignUpGroupIsDisplayed()
+
         emailEditText.check(matches(hasErrorText(invalidEmailError)))
+    }
+
+    @Test
+    fun testNavigationToSignInFragment() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+
+        scenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        signUpTextView.perform(click())
+        assertEquals(navController.currentDestination?.id, R.id.signUpFragment)
+    }
+
+    private fun checkSignUpGroupIsDisplayed() {
+        emailEditText.check(matches(isDisplayed()))
+        passwordEditText.check(matches(isDisplayed()))
     }
 
     companion object {
